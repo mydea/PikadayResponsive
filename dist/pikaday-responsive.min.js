c7cc383c3fd9bd2adea157b166656f892437c810
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-02-19 21:06:48 -0500

    use grunt-modernizr instead of the full (bower) modernizr

--- dist/pikaday-responsive.min.js
+++ dist/pikaday-responsive.min.js
@@ -1,4 +1,5 @@
-/*! 
+
+ /* 
  * PikadayResponsive 
  * A responsive datepicker built on top of Pikaday. It shows the native datepicker on mobile devices and a nice JS-picker on desktop. 
  * 
--- gruntfile.js
+++ gruntfile.js
@@ -1,10 +1,17 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
     // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        banner: "/*! \n * PikadayResponsive \n * A responsive datepicker built on top of Pikaday. It shows the native datepicker on mobile devices and a nice JS-picker on desktop. \n * \n * @author: Francesco Novy \n * @licence: MIT <http://www.opensource.org/licenses/mit-license.php> \n * @link https://github.com/mydea/PikadayResponsive \n * @copyright: (c) <%= grunt.template.today('yyyy') %> \n * @version: <%= pkg.version %> \n */ \n\n",
+        banner: "\n /* \n * PikadayResponsive \n * A responsive datepicker built on top of Pikaday. It shows the native datepicker on mobile devices and a nice JS-picker on desktop. \n * \n * @author: Francesco Novy \n * @licence: MIT <http://www.opensource.org/licenses/mit-license.php> \n * @link https://github.com/mydea/PikadayResponsive \n * @copyright: (c) <%= grunt.template.today('yyyy') %> \n * @version: <%= pkg.version %> \n */ \n\n",
 
+        modernizr: {
+            dist: {
+                "dest": "dist/pikaday-responsive-modernizr.js",
+                "devFile": "src/pikaday-responsive.js",
+                "uglify": true
+            }
+        },
         concat: {
             options: {
                 banner: "<%= banner %>"
@@ -36,8 +43,7 @@ module.exports = function (grunt) {
             dist: {
                 options: {
                     style: 'expanded',
-                    sourcemap: "none",
-                    banner: "<%= banner %>"
+                    sourcemap: "none"
                 },
                 files: {
                     'dist/pikaday-responsive.css': 'src/css/scss/pikaday-responsive.scss'
@@ -48,11 +54,12 @@ module.exports = function (grunt) {
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks("grunt-modernizr");
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ["sass", "uglify", "concat"]);
+    grunt.registerTask('default', ["sass", "uglify", "concat", "modernizr"]);
 
 };
