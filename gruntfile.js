2435a9ae08a1167870f8db0b9f3cc7babc1f61d1
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-02-19 22:12:50 -0500

    added cssmin; built new dist folder

--- gruntfile.js
+++ gruntfile.js
@@ -7,36 +7,69 @@ module.exports = function(grunt) {
 
         modernizr: {
             dist: {
-                "dest": "dist/pikaday-responsive-modernizr.js",
+                "dest": "dist/dependencies/pikaday-responsive-modernizr.js",
                 "devFile": "src/pikaday-responsive.js",
                 "uglify": true
             }
         },
-        concat: {
-            options: {
-                banner: "<%= banner %>"
-            },
-            copy: {
-                src: ["src/pikaday-responsive.js"],
-                dest: "dist/pikaday-responsive.js"
-            },
-            app: {
-                src: ['bower_components/momentjs/moment.js', 'bower_components/pikaday/pikaday.js', 'src/pikaday-responsive.js'],
-                dest: 'dist/pikaday-package.js'
-            },
-            css: {
-                src: ["bower_components/pikaday/css/pikaday.css", "dist/pikaday-responsive.css"],
-                dest: "dist/pikaday-package.css"
+        cssmin: {
+            minify: {
+                files: {
+                    'dist/package_components/pikaday-responsive.min.css': ['dist/package_components/pikaday-responsive.css'],
+                    'dist/pikaday-package.min.css': ['dist/pikaday-package.css'],
+                    'dist/package_components/pikaday.min.css': ['dist/package_components/pikaday.css']
+                }
             }
         },
         uglify: {
             options: {
-                mangle: false,
-                banner: "<%= banner %>"
+                mangle: false
             },
-            pikadayResponsive: {
-                src: 'src/pikaday-responsive.js',
-                dest: 'dist/pikaday-responsive.min.js'
+            pickADayResponsive: {
+                options: {
+                    banner: "<%= banner %>"
+                },
+                files: {
+                    'dist/package_components/pikaday-responsive.min.js': ['dist/package_components/pikaday-responsive.js'],
+                    'dist/pikaday-package.min.js': ['dist/pikaday-package.js']
+                }
+            },
+            vendor: {
+                files: {
+                    'dist/package_components/moment-timezone.min.js': ['dist/package_components/moment-timezone.js'],
+                    'dist/package_components/pikaday-responsive.min.js': ['dist/package_components/pikaday-responsive.js'],
+                    'dist/package_components/pikaday.min.js': ['dist/package_components/pikaday.js']
+                }
+            }
+        },
+        concat: {
+            lib: {
+                options: {
+                    banner: "<%= banner %>"
+                },
+                src: ["src/pikaday-responsive.js"],
+                dest: "dist/package_components/pikaday-responsive.js"
+            },
+            dependencies: {
+                files: {
+                    "dist/package_components/pikaday.js": ["bower_components/pikaday/pikaday.js"],
+                    "dist/package_components/pikaday.css": ["bower_components/pikaday/css/pikaday.css"],
+                    "dist/package_components/moment.js": ["bower_components/moment/moment.js"],
+                    "dist/package_components/moment.min.js": ["bower_components/moment/min/moment.min.js"],
+                    "dist/package_components/moment-timezone.js": ["bower_components/moment-timezone/moment-timezone.js"],
+                    "dist/dependencies/jquery.min.js": ["bower_components/jquery/dist/jquery.min.js"],
+                }
+            },
+            app: {
+                options: {
+                    banner: "<%= banner %>"
+                },
+                src: ['dist/package_components/moment.js', 'dist/package_components/pikaday.js', 'dist/package_components/pikaday-responsive.js'],
+                dest: 'dist/pikaday-package.js'
+            },
+            css: {
+                src: ["dist/package_components/pikaday.css", "dist/package_components/pikaday-responsive.css"],
+                dest: "dist/pikaday-package.css"
             }
         },
         sass: {
@@ -46,7 +79,7 @@ module.exports = function(grunt) {
                     sourcemap: "none"
                 },
                 files: {
-                    'dist/pikaday-responsive.css': 'src/css/scss/pikaday-responsive.scss'
+                    'dist/package_components/pikaday-responsive.css': 'src/css/scss/pikaday-responsive.scss'
                 }
             }
         }
@@ -55,11 +88,12 @@ module.exports = function(grunt) {
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks("grunt-modernizr");
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-sass');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ["sass", "uglify", "concat", "modernizr"]);
+    grunt.registerTask('default', ["sass", "concat", "uglify", "cssmin", "modernizr"]);
 
 };
